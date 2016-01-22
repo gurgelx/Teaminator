91ce16cd1c8139ab19bc4d98fc1c2f7e6bdb7ac9
Gustaf Brahme <gustav.brahme@hm.com>
2016-01-22 16:59:01 +0100

    Added events to trigger new teamcity builds etc

--- WebApi/Teaminator.TeamcityService/TeamcityClient.cs
+++ WebApi/Teaminator.TeamcityService/TeamcityClient.cs
@@ -5,6 +5,10 @@ namespace Teaminator.TeamcityService
 {
     public class TeamcityClient
     {
+        public event EventHandler<EventArgs> BuildError;
+        public event EventHandler<EventArgs> BuildSuccess;
+        public event EventHandler<EventArgs> BuildRunning;
+
         private const string builds = "httpAuth/app/rest/builds/";
         private readonly string host;
         public TeamcityClient(string host)
